e8edf8bc23c0c90b9e14aaafeedb150e86eb91cf
mrunal93 <mrn26893@gmail.com>
2020-07-24 07:15:11 +0530

    [Mrunal]Add: welcome Message for mood analizer

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalysis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalysis
+    {
+        private string message;
+        public MoodAnalysis (string message)
+        {
+
+        }
+    }
+}
